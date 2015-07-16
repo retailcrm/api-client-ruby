1f2cd4128e31b86e13c46a2743364b103e63bfe8
Alex Lushpai <lushpai@gmail.com>
2015-07-16 18:03:57 +0300

    enable specs for refs

--- spec/spec_references.rb
+++ spec/spec_references.rb
@@ -16,24 +16,24 @@ describe Retailcrm do
     @ref = @bootstrap.refs_edit
   end
 
-  # describe '#reference get' do
-  #   it 'responds with 200' do
-  #     @bootstrap.refs_get.each { |method|
-  #       res = @api.public_send(method)
-  #       res.status.to_i.must_equal 200
-  #       sleep 0.3
-  #     }
-  #   end
-  # end
-  #
-  # describe '#reference edit' do
-  #   it 'responds with 200 or 201' do
-  #     @bootstrap.refs_edit.each do |method, data|
-  #       res = @api.public_send(method, data)
-  #       res.status.to_i.must_be :<, 202
-  #       sleep 0.3
-  #     end
-  #   end
-  # end
+  describe '#reference get' do
+    it 'responds with 200' do
+      @bootstrap.refs_get.each { |method|
+        res = @api.public_send(method)
+        res.status.to_i.must_equal 200
+        sleep 0.3
+      }
+    end
+  end
+
+  describe '#reference edit' do
+    it 'responds with 200 or 201' do
+      @bootstrap.refs_edit.each do |method, data|
+        res = @api.public_send(method, data)
+        res.status.to_i.must_be :<, 202
+        sleep 0.3
+      end
+    end
+  end
 
 end
